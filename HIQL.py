Refactor network module, finish integrating dataset/batches, fix numerical issues

HIQL.py
@@ -1,67 +1,76 @@
+import flax
 import jax
 import jax.numpy as jnp
-from batch_gen import Batch
-import config
-from nets import LowPolicy, HighPolicy, ValueNet
+from batch_gen import Batch, Dataset
+from config import cfg
+from networks.nets import LowPolicy, HighPolicy, ValueNet
 import util as utils
-import optax 
+import optax
+import functools as ft
+from typing import Any
 
-class HIQLAgent:
+class HIQLAgent(flax.struct.PyTreeNode):
+    train_state: utils.TrainState
+    dataset: Dataset
+
     # expectile loss: MSE, but weight adv >= 0 differently than adv < 0
-    def expectile_loss(ME, adv, expectile = config.expectile):
+    def expectile_loss(self, ME, adv, expectile = cfg.expectile):
         weight = jnp.where(adv >= 0, expectile, (1 - expectile))
         return weight * (ME ** 2)
 
     @jax.jit
-    def high_actor_loss(self, batch: Batch): # standard A2C loss method
-        print(batch.high_actor_goals)
+    def high_actor_loss(self, params, batch: Batch): # standard A2C loss method
+        high_actor_net = ft.partial(self.train_state.select("high_lvl_policy"), params=params)
 
     @jax.jit
-    def low_actor_loss(self, batch: Batch): # standard A2C loss method
-        pass
+    def low_actor_loss(self, params, batch: Batch): # standard A2C loss method
+        low_actor_net = ft.partial(self.train_state.select("low_lvl_policy"), params=params)
 
     @jax.jit
-    def value_loss(self, batch: Batch):
-        pass
+    def value_loss(self, params, batch: Batch):
+        value_net = ft.partial(self.train_state.select("value_net"), params=params)
+        target_net = ft.partial(self.train_state.select("target_net"), params=params)
 
 
     @jax.jit
-    def total_loss(self, batch: Batch):
+    def total_loss(self, params, batch: Batch):
         pass
-        
 
-    def create(self):
-        rng = jax.random.PRNGKey(config.seed)
+    @classmethod
+    def create(cls, dataset):
+        rng = jax.random.PRNGKey(cfg.seed)
         rng, init_rng = jax.random.split(rng, 2)
-        
-        self.networks = {
-            "value_net": ValueNet(hidden_dims=(5, 6, 7)),
-            "target_net": ValueNet(hidden_dims=(5, 6, 7)),
-            "low_lvl_policy": LowPolicy(hidden_dims=(5, 6, 7)),
-            "high_lvl_policy": HighPolicy(hidden_dims=(5, 6, 7)),
+
+        networks = {
+            "value_net": ValueNet(hidden_dims=cfg.value_HD),
+            "target_net": ValueNet(hidden_dims=cfg.value_HD),
+            "low_lvl_policy": LowPolicy(hidden_dims=cfg.low_actor_HD),
+            "high_lvl_policy": HighPolicy(hidden_dims=cfg.high_actor_HD),
         }
+
+        dataset_gen = Dataset.create(**dataset)
+        setup_batch = dataset_gen.sample(1)
+
         network_args = {
-            "value_net": (jnp.ones((5, 7)), jnp.ones((5, 7))),
-            "target_net": (jnp.ones((7, 9)), jnp.ones((7, 9))),
-            "low_lvl_policy": (jnp.ones((8, 10)), jnp.ones((8, 10))),
-            "high_lvl_policy": (jnp.ones((9, 11)), jnp.ones((9, 11)))
+            "value_net": (setup_batch.obs, setup_batch.value_goals),
+            "target_net": (setup_batch.obs, setup_batch.value_goals),
+            "low_lvl_policy": (setup_batch.obs, setup_batch.low_actor_goals),
+            "high_lvl_policy": (setup_batch.obs, setup_batch.high_actor_goals)
         }
-        
-        networks = utils.ModuleDict(self.networks)
-        network_optim = optax.adam(learning_rate=config.lr)
+
+        networks = utils.ModuleDict(networks)
+        network_optim = optax.adam(learning_rate=cfg.lr)
 
         params = networks.init(init_rng, **network_args)["params"]
 
-        network = utils.TrainState.create(networks, params, network_optim, {})
-        print(network.select("value_net")(jnp.ones((5, 7)), jnp.ones((5, 7))))
-        network.select("low_lvl_policy")(jnp.ones((8, 10)), jnp.ones((8, 10)))
-    
-    def sample_actions(self, obs): 
+        network = utils.TrainState.create(networks, params, network_optim)
+        return cls(train_state=network, dataset=dataset_gen)
+
+
+    def sample_actions(self, obs):
         pass
-    
+
     def update(self):
+        # every 50 steps, update the target network
         pass
     
-    
-hiql_agent = HIQLAgent()
-hiql_agent.create()

batch_gen.py
@@ -3,6 +3,7 @@ import jax, jax.numpy as jnp
 import ogbench
 from flax.core.frozen_dict import FrozenDict
 import numpy as np
+from config import cfg
 
 dataset_name = 'antmaze-large-navigate-v0'
 env, train_dataset, val_dataset = ogbench.make_env_and_datasets(
@@ -14,6 +15,8 @@ env, train_dataset, val_dataset = ogbench.make_env_and_datasets(
 Batch = namedtuple("Batch",
                    "obs "
                    "next_obs "
+                   "masks "
+                   "terminals "
                    "rewards "
                    "value_goals "
                    "low_actor_goals "
@@ -33,13 +36,13 @@ class Dataset(FrozenDict):
     def create(cls, **kwargs):
         assert 'observations' in kwargs and 'next_observations' in kwargs
         jax.tree_util.tree_map(lambda arr: arr.setflags(write=False), kwargs)
-        return cls(kwargs)
+        return cls(**kwargs)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.data = kwargs  # the dict with all the actual data
         self.size = get_size(self.data)
-        self.terminal_locations = np.where(
+        (self.terminal_idx,) = np.where(
             self.data["terminals"] == 1)  # indices of terminals; to avoid bugs in goal select
 
     # random indices in range
@@ -58,14 +61,99 @@ class Dataset(FrozenDict):
     def get_observations(self, idxs):
         return jax.tree_util.tree_map(lambda arr: arr[idxs], self.data['observations'])
 
-    def sample_goals(self, amt, ):
-        pass
+    # these methods are mostly copied from the original HIQL, as there is not much to change
+    def sample_goals(self, amt, idxs, p_curgoal, p_trajgoal, geo_sampling: bool = False):
+        random_g_idx = self.get_random_idx(amt)
+
+        # goals from current trajectory
+        final_state_idxs = self.terminal_idx[np.searchsorted(self.terminal_idx, idxs)]
+        # geometric:
+        if geo_sampling:
+            offsets = np.random.geometric(p=1 - cfg.discount, size=amt)  # in [1, inf)
+            traj_goal_idxs = np.minimum(idxs + offsets, final_state_idxs)
+        else:
+            # uniform:
+            distances = np.random.rand(amt)  # in [0, 1)
+            traj_goal_idxs = np.round(
+                (np.minimum(idxs + 1, final_state_idxs) * distances + final_state_idxs * (1 - distances))
+            ).astype(int)
+
+        goal_idxs = np.where(
+            np.random.rand(amt) < p_trajgoal / (1.0 - p_curgoal + 1e-6), traj_goal_idxs, random_g_idx
+        )
+        # Goals at the current state.
+        goal_idxs = np.where(np.random.rand(amt) < p_curgoal, idxs, goal_idxs)
+
+        return goal_idxs
 
     def sample(self, amt, idxs=None) -> Batch:
         if not idxs:
             idxs = self.get_random_idx(amt)
 
+        raw_batch = self.sampleTransitions(amt)
+        batch = {
+            "obs": raw_batch["observations"],
+            "next_obs": raw_batch["next_observations"],
+            "actions": raw_batch["actions"],
+            "terminals": raw_batch["terminals"]
+        }
+
+        # VF goals
+        value_goal_idxs = self.sample_goals(
+            amt,
+            idxs,
+            cfg.V_p_curgoal,
+            cfg.V_p_trajgoal,
+            cfg.V_geom_sample
+        )
+        batch["value_goals"] = self.get_observations(value_goal_idxs)
+
+        successes = (idxs == value_goal_idxs).astype(float)
+        batch["masks"] = 1.0 - successes
+        batch["rewards"] = successes - (1.0 if cfg.gc_negative else 0.0)
+
+        # low level actor goals
+        final_state_idxs = self.terminal_idx[np.searchsorted(self.terminal_idx, idxs)]
+        low_goal_idxs = np.minimum(idxs + cfg.subgoal_steps, final_state_idxs)
+        batch['low_actor_goals'] = self.get_observations(low_goal_idxs)
+
+        if cfg.A_geom_sample:
+            # Geometric sampling.
+            offsets = np.random.geometric(p=1 - cfg.discount, size=amt)  # in [1, inf)
+            high_traj_goal_idxs = np.minimum(idxs + offsets, final_state_idxs)
+        else:
+            # Uniform sampling.
+            distances = np.random.rand(amt)  # in [0, 1)
+            high_traj_goal_idxs = np.round(
+                (np.minimum(idxs + 1, final_state_idxs) * distances + final_state_idxs * (1 - distances))
+            ).astype(int)
+
+        high_traj_target_idxs = np.minimum(idxs + cfg.subgoal_steps, high_traj_goal_idxs)
+
+        # High-level random goals.
+        high_random_goal_idxs = self.get_random_idx(amt)
+        high_random_target_idxs = np.minimum(idxs + cfg.subgoal_steps, final_state_idxs)
+
+        # Pick between high-level future goals and random goals.
+        pick_random = np.random.rand(amt) < cfg.A_p_randomgoal
+        high_goal_idxs = np.where(pick_random, high_random_goal_idxs, high_traj_goal_idxs)
+        high_target_idxs = np.where(pick_random, high_random_target_idxs, high_traj_target_idxs)
+
+        batch["high_actor_goals"] = self.get_observations(high_goal_idxs)
+        batch["high_actor_targets"] = self.get_observations(high_target_idxs)
 
         return Batch(
-
+            obs=batch["obs"],
+            next_obs=batch["next_obs"],
+            masks=batch["masks"],
+            terminals=batch["terminals"],
+            rewards=batch["rewards"],
+            value_goals=batch["value_goals"],
+            low_actor_goals=batch["low_actor_goals"],
+            high_actor_goals=batch["high_actor_goals"],
+            high_actor_targets=batch["high_actor_targets"],
+            actions=batch["actions"]
         )
+
+dataset = Dataset.create(**train_dataset)
+l = dataset.sample(3)

networks/mods.py
@@ -1,12 +1,22 @@
 import flax.linen as nn
 from typing import Any
+from config import cfg
 
 # for now, all networks will be basic MLPs
 # However, will add all the bells and whistles once I get the basic implementation done
 
 # ensemblize; not sure how to implement
-def ensemblize():
-    pass
+def ensemblize(cls, num_qs, out_axes=0, **kwargs):
+    """Ensemblize a module."""
+    return nn.vmap(
+        cls,
+        variable_axes={'params': 0},
+        split_rngs={'params': True},
+        in_axes=None,
+        out_axes=out_axes,
+        axis_size=num_qs,
+        **kwargs,
+    )
 
 class MLP(nn.Module):
     
@@ -23,3 +33,5 @@ class MLP(nn.Module):
                 if self.layer_norm:
                     x = nn.LayerNorm()(x)
         return x
+
+MLP = ensemblize(MLP, cfg.num_ensemble)

networks/nets.py
@@ -1,6 +1,6 @@
 import flax.linen as nn
 from typing import Any
-from mods import MLP
+from networks.mods import MLP
 import jax.numpy as jnp
 
 
@@ -14,7 +14,7 @@ class LowPolicy(nn.Module): # state, subgoal => action
         self.low_policy = MLP(self.hidden_dims, self.activation, self.layer_norm)
     
     def __call__(self, obs, subgoals):
-        return self.low_policy(jnp.append(obs, subgoals))
+        return self.low_policy(jnp.append(obs, subgoals, axis=1))
 
 class HighPolicy(nn.Module): # state, goal => subgoal
     hidden_dims: tuple[int, ...]
@@ -26,7 +26,7 @@ class HighPolicy(nn.Module): # state, goal => subgoal
         self.high_policy = MLP(self.hidden_dims, self.activation, self.layer_norm)
         
     def __call__(self, obs, goals):
-        return self.high_policy(jnp.append(obs, goals))
+        return self.high_policy(jnp.append(obs, goals, axis=1))
 
 
 class ValueNet(nn.Module): # state, (sub)goal (rep) => value
@@ -41,8 +41,8 @@ class ValueNet(nn.Module): # state, (sub)goal (rep) => value
     def __call__(self, obs, goals):
         if self.goal_rep:
             pass
-        # something 
-        return self.value_net(jnp.append(obs, goals))
+
+        return self.value_net(jnp.append(obs, goals, axis=1))
 
 
 class GoalRep(nn.Module): # (sub)goal => (sub)goal rep